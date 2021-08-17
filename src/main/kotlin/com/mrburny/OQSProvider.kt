f9a898504f25b735638149ca61cfea20c4d96b9b
Viktor Sergeev <mrburny.sv@gmail.com>
2021-08-17 20:11:44 +0200

    new path to signature SPI

--- src/main/kotlin/com/mrburny/OQSProvider.kt
+++ src/main/kotlin/com/mrburny/OQSProvider.kt
@@ -7,7 +7,10 @@ object OQSProvider : Provider("OQS", 1.0, "Provider of OQS implementations of PQ
         put("KeyFactory.Dilithium2-AES", "com.mrburny.dilithium.Dilithium2AESKeyFactorySpi")
         put("KeyPairGenerator.Dilithium2-AES", "com.mrburny.dilithium.Dilithium2AESKeyPairGeneratorSpi")
         put("Signature.Dilithium2-AES", "com.mrburny.dilithium.Dilithium2AESSignatureSpi")
+
         //TODO: private key containing OID will fail to find it by algorithm name
+        put("KeyFactory.1.3.6.1.4.1.2.267.11.4.4", "com.mrburny.dilithium.Dilithium2AESKeyFactorySpi")
+        put("KeyPairGenerator.1.3.6.1.4.1.2.267.11.4.4", "com.mrburny.dilithium.Dilithium2AESKeyPairGeneratorSpi")
         put("Signature.1.3.6.1.4.1.2.267.11.4.4", "com.mrburny.dilithium.Dilithium2AESSignatureSpi")
     }
 
